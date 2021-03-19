8e6a7d046ea9f0ac9f246fc461db09f301abdd5f
hashika <m.g.hashikamaduranga@gmail.com>
2021-03-19 09:22:48 +0530

    changes in whitespace

--- server/deliveries/controller/auth/index.js
+++ server/deliveries/controller/auth/index.js
@@ -1,20 +1,20 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { Register,Login,Logout } = require('./Auth');
+const { Register, Login, Logout } = require("./Auth");
 
-router.post('/',(req, res) => {
-    const register = Register(req,res);
-    return register;    
-});   
-
-router.post('/login',(req, res) => { 
-  const login = Login(req,res);
-  return login;    
+router.post("/", (req, res) => {
+  const register = Register(req, res);
+  return register;
 });
 
-router.post('/logout',(req, res) => { 
-  const logout = Logout(req,res);
-  return logout;    
+router.post("/login", (req, res) => {
+  const login = Login(req, res);
+  return login;
+});
+
+router.post("/logout", (req, res) => {
+  const logout = Logout(req, res);
+  return logout;
 });
 
 module.exports = router;
