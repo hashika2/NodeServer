8769c1e0a30d210cf6894a4dea6d6cbde0dd2105
hashika <m.g.hashikamaduranga@gmail.com>
2021-06-26 22:58:07 +0530

    add constant data

--- server/shared/environment/config.js
+++ server/shared/environment/config.js
@@ -1,4 +1,9 @@
-module.exports= {
-    ClientId:'5jtq552irtgporan8k762k9rac',
-    UserPoolId:'us-east-1_0iu3pyUxb'
-}
+module.exports = {
+  ClientId: "5jtq552irtgporan8k762k9rac",
+  UserPoolId: "us-east-1_0iu3pyUxb",
+  Email: {
+    user: "testmail@codexlabstechnologies.com",
+    pass: "TestMail@12345",
+  },
+  BaseUrl:"http://localhost:3000"
+};
